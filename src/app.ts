7b4809481b9c4d178d3a20ea31f8e6bb58b06e97
AaronMontes2004 <montesaaron2018@gmail.com>
2023-03-23 14:16:12 -0500

    Modified limit

--- src/app.ts
+++ src/app.ts
@@ -29,8 +29,8 @@ app.set("PORT", process.env.PORT || 4000);
 // Middlewares
 app.use(cors())
 app.use(morgan("dev"))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({extended: false, limit: '50mb'}))
 app.use(passport.initialize())
 passport.use(strategyPassport)
 
