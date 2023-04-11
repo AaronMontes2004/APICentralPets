0f255c2ae987153fb716472cb44bf7af99f05b0f
AaronMontes2004 <montesaaron2018@gmail.com>
2023-04-11 13:52:06 -0500

    Modified

--- src/libs/queriesValidation/brandQueryValidation.ts
+++ src/libs/queriesValidation/brandQueryValidation.ts
@@ -1,5 +1,5 @@
 export const verifyNameBrand: string = "SELECT * FROM marca WHERE nombreMarca = ?"
 
-export const verifyIdBrand: string = "SELECT * FROM marca WHERE idMarca = ? WHERE estadoMarca = 1"
+export const verifyIdBrand: string = "SELECT * FROM marca WHERE idMarca = ? AND estadoMarca = 1"
 
 export const verifyAllIdBrand: string = "SELECT * FROM marca WHERE idMarca = ?"
