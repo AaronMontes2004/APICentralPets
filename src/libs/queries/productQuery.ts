df9621a1d0d98a9014cb552b7db512f27a9dd075
AaronMontes2004 <montesaaron2018@gmail.com>
2023-04-15 20:43:41 -0500

    Modified

--- src/libs/queries/productQuery.ts
+++ src/libs/queries/productQuery.ts
@@ -1,4 +1,4 @@
-export const getProductsQuery: string = "SELECT * FROM producto WHERE estadoProducto = 1"
+export const getProductsQuery: string = "SELECT * FROM producto p INNER JOIN categoria c ON p.idCategoria = c.idCategoria WHERE p.estadoProducto = 1"
 
 export const addProductQuery: string = "INSERT INTO producto(nombreProducto, descripcionProducto, stockProducto, precioProducto, imagenProducto, idCategoria, idMarca) VALUES (?,?,?,?,?,?,?)"
 
@@ -6,9 +6,9 @@ export const updateProductQuery: string = "UPDATE producto SET nombreProducto = 
 
 export const updateProductAndroidQuery: string = "UPDATE producto SET nombreProducto = ?, descripcionProducto = ?, stockProducto = ?, precioProducto = ?, idCategoria = ?, idMarca = ?, imagenProducto = ? WHERE idProducto = ?"
 
-export const findByIdProductQuery: string = "SELECT * FROM producto WHERE idProducto = ? AND estadoProducto = 1"
+export const findByIdProductQuery: string = "SELECT * FROM producto p INNER JOIN categoria c ON p.idCategoria = c.idCategoria WHERE p.idProducto = ? AND p.estadoProducto = 1"
 
-export const findProductsByIdCategoryQuery: string = "SELECT * FROM producto WHERE idCategoria = ? AND estadoProducto = 1"
+export const findProductsByIdCategoryQuery: string = "SELECT * FROM producto p INNER JOIN categoria c ON p.idCategoria = c.idCategoria WHERE p.idCategoria = ? AND p.estadoProducto = 1"
 
 export const findAllByIdProductQuery: string = "SELECT * FROM producto WHERE idProducto = ?"
 
