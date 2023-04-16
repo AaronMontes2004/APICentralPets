79fd4c44f0b1f07cd75c09cdca6074facd8477ea
AaronMontes2004 <montesaaron2018@gmail.com>
2023-04-15 22:48:16 -0500

    Modified

--- src/libs/queries/medicalHistoryQuery.ts
+++ src/libs/queries/medicalHistoryQuery.ts
@@ -4,4 +4,4 @@ export const addMedicalHistoryQuery: string = "INSERT INTO historial_medico(fech
 
 export const updateMedicalHistoryQuery: string = "UPDATE historial_medico SET motivoHistorial = ?, idDiagnostico = ?, idTratamiento = ?, idUsuario = ?, idMascota = ? WHERE idHistorial = ?"
 
-export const findByIdMedicalHistoryQuery: string = "SELECT * FROM historial_medico WHERE idHistorial = ?"
+export const findByIdMedicalHistoryQuery: string = "SELECT * FROM historial_medico hm INNER JOIN mascota m ON hm.idMascota = m.idMascota INNER JOIN tratamiento t ON hm.idTratamiento = t.idTratamiento INNER JOIN diagnostico d ON hm.idDiagnostico = d.idDiagnostico WHERE idHistorial = ?"
