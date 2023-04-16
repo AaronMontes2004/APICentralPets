fb0ddf046c301481e245be709acc238aafa16a27
AaronMontes2004 <montesaaron2018@gmail.com>
2023-04-16 17:33:24 -0500

    Modified

--- src/libs/queries/vetQuery.ts
+++ src/libs/queries/vetQuery.ts
@@ -1,4 +1,4 @@
-export const getVetsQuery: string = "SELECT * FROM veterinario WHERE estadoVeterinario = 1"
+export const getVetsQuery: string = "SELECT * FROM veterinario v INNER JOIN especialidad e ON v.idEspecialidad = e.idEspecialidad INNER JOIN sexo s ON v.idSexo = s.idSexo WHERE estadoVeterinario = 1"
 
 export const addVetQuery: string = "INSERT INTO veterinario(nombreVeterinario, apellidoVeterinario, dniVeterinario, telefonoVeterinario, direccionVeterinario, correoVeterinario, fotoVeterinario, idEspecialidad, idSexo) VALUES (?,?,?,?,?,?,?,?,?)"
 
@@ -6,8 +6,8 @@ export const updateVetQuery: string = "UPDATE veterinario SET nombreVeterinario 
 
 export const updateVetAndroidQuery: string = "UPDATE veterinario SET nombreVeterinario = ?, apellidoVeterinario = ?, dniVeterinario = ?, telefonoVeterinario = ?, direccionVeterinario = ?, correoVeterinario = ?, idEspecialidad = ?, idSexo = ?, fotoVeterinario = ? WHERE idVeterinario = ?"
 
-export const findByIdVetQuery: string = "SELECT * FROM veterinario WHERE idVeterinario = ? AND estadoVeterinario = 1"
+export const findByIdVetQuery: string = "SELECT * FROM veterinario v INNER JOIN especialidad e ON v.idEspecialidad = e.idEspecialidad INNER JOIN sexo s ON v.idSexo = s.idSexo WHERE idVeterinario = ? AND estadoVeterinario = 1"
 
-export const findAllByIdVetQuery: string = "SELECT * FROM veterinario WHERE idVeterinario = ?"
+export const findAllByIdVetQuery: string = "SELECT * FROM veterinario v INNER JOIN especialidad e ON v.idEspecialidad = e.idEspecialidad INNER JOIN sexo s ON v.idSexo = s.idSexo WHERE idVeterinario = ?"
 
 export const changeStatusVetQuery: string = "UPDATE veterinario SET estadoVeterinario = ? WHERE idVeterinario = ?"
