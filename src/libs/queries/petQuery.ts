4ed0237b2384d564a60a00ac17ac645ef38c07eb
AaronMontes2004 <montesaaron2018@gmail.com>
2023-04-16 01:09:54 -0500

    Modified

--- src/libs/queries/petQuery.ts
+++ src/libs/queries/petQuery.ts
@@ -1,4 +1,4 @@
-export const getPetsQuery: string = "SELECT * FROM mascota WHERE estadoMascota = 1"
+export const getPetsQuery: string = "SELECT * FROM mascota m INNER JOIN especie e ON m.idEspecie = e.idEspecie WHERE estadoMascota = 1"
 
 export const addPetQuery: string = "INSERT INTO mascota(nombreMascota, edadMascota, sexoMascota, pesoMascota, fotoMascota, idEspecie, idUsuario) VALUES (?,?,?,?,?,?,?)"
 
@@ -6,10 +6,10 @@ export const updatePetQuery: string = "UPDATE mascota SET nombreMascota = ?, eda
 
 export const updatePetAndroidQuery: string = "UPDATE mascota SET nombreMascota = ?, edadMascota = ?, sexoMascota = ?, pesoMascota = ?, idEspecie = ?, idUsuario = ?, fotoMascota = ? WHERE idMascota = ?"
 
-export const findByIdPetQuery: string = "SELECT * FROM mascota WHERE idMascota = ? AND estadoMascota = 1"
+export const findByIdPetQuery: string = "SELECT * FROM mascota m INNER JOIN especie e ON m.idEspecie = e.idEspecie WHERE idMascota = ? AND estadoMascota = 1"
 
-export const findAllByIdPetQuery: string = "SELECT * FROM mascota WHERE idMascota = ?"
+export const findAllByIdPetQuery: string = "SELECT * FROM mascota m INNER JOIN especie e ON m.idEspecie = e.idEspecie WHERE idMascota = ?"
 
 export const changeStatusPetQuery: string = "UPDATE mascota SET estadoMascota = ? WHERE idMascota = ?"
 
-export const findPetsByIdUserQuery: string = "SELECT * FROM mascota WHERE idUsuario = ? AND estadoMascota = 1"
+export const findPetsByIdUserQuery: string = "SELECT * FROM mascota m INNER JOIN especie e ON m.idEspecie = e.idEspecie WHERE idUsuario = ? AND estadoMascota = 1"
